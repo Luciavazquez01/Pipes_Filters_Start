535448fd3cbfda4c020c35f1c35f331dadc61151
Lucia Vazquez <luciavazquezreyes299@gmail.com>
2023-10-25 08:20:55 -0300

    ..

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -4,10 +4,28 @@ using CompAndDel.Filters;
 
 namespace CompAndDel
 {
-    class Program
+     class Program
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+
+            PipeNull pnull = new PipeNull();
+            FilterSaveImage resParcial2 = new FilterSaveImage(@"resParcial2.jpg");
+            PipeSerial rp2 = new PipeSerial(resParcial2, pnull);
+            FilterNegative negative = new FilterNegative();
+            PipeSerial serial2 = new PipeSerial(negative, rp2);
+            FilterTwitter ft = new FilterTwitter(@"resParcial1.jpg", "ej3");
+            PipeSerial t1 = new PipeSerial(ft, serial2);
+            FilterSaveImage resParcial1 = new FilterSaveImage(@"resParcial1.jpg");
+            PipeSerial rp1 = new PipeSerial(resParcial1, t1);
+            FilterGreyscale greyscale = new FilterGreyscale();
+            PipeSerial serial1 = new PipeSerial(greyscale, rp1);
+
+            IPicture s1 = serial1.Send(picture);
+
+            provider.SavePicture(s1, @"beerEdited.jpg");
         }
     }
 }
